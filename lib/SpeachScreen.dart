17f6e4c67b1069a8cc95cd39117cc4071b54fd01
sapperberet <ahmedamromran2003@gmail.com>
2024-03-24 01:29:11 +0200

    first

---
+++ lib/SpeachScreen.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class SpeachScreen extends StatefulWidget {
+  const SpeachScreen({super.key});
+
+  @override
+  State<SpeachScreen> createState() => _SpeachScreenState();
+}
+
+class _SpeachScreenState extends State<SpeachScreen> {
+
+
+  bool _isListening = false;
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
